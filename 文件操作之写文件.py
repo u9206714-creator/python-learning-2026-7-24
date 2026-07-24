26394351784e2f4b1efffe05b40e138c930212de
u9206714-creator <u9206714@gmail.com>
2026-07-24 22:17:12 +0800

    学习如何写文件

---
+++ 文件操作之写文件.py
@@ -0,0 +1,17 @@
+# task1
+with open(r'C:\Users\HUAWEI\Desktop\poem.txt','w', encoding='utf-8') as f:
+    f.write('我欲乘风归去，\n')
+    f.write('又恐琼楼玉宇，\n')
+    f.write('高处不胜寒。\n')
+
+
+# task2
+with open(r'C:\Users\HUAWEI\Desktop\poem.txt','a', encoding='utf-8') as f:
+    f.write('起舞弄清影，\n')
+    f.write('何似在人间。')
+
+
+
+
+
+
